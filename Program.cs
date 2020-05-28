127283c7d553b3f74ebb0683475e4e25a091df86
Kristen Howton <kristen.howton@gmail.com>
2020-05-27 21:20:03 -0500

    set up classes, methods, and contucts

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Planner
+{
+    class Building
+    {
+        private string _designer;
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+
+        }
+        //this is a constructor
+        public Building(string address)
+        {
+            _address = address;
+        }
+        //these are methods
+        public void Purchase(string name)
+        {
+            _owner = name;
+        }
+
+        public void Constuct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
